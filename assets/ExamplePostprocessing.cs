4cca3c4a9491d967026d7e43b2beecf46816e627
wheatleymf <wheatleyinthespace@yandex.ru>
2025-01-17 02:11:10 +0700

    basic postprocessing sample material

---
+++ assets/ExamplePostprocessing.cs
@@ -0,0 +1,34 @@
+﻿namespace Sandbox;
+
+[Title( "My Post Processing" )]
+[Category( "Post Processing" )]
+[Icon( "grain" )]
+public sealed class MyPostProcessing : PostProcess, Component.ExecuteInEditor
+{
+	[Property] [Range( 0, 3 )] public float Brightness { get; set; } = 1.0f;
+
+	private IDisposable _renderHook;
+
+	protected override void OnEnabled()
+	{
+		_renderHook = Camera.AddHookBeforeOverlay( "My Post Processing", 1000, RenderEffect );
+	}
+
+	protected override void OnDisabled()
+	{
+		_renderHook?.Dispose();
+		_renderHook = null;
+	}
+
+	RenderAttributes attributes = new RenderAttributes();
+
+	public void RenderEffect( SceneCamera camera )
+	{
+		if ( !camera.EnablePostProcessing )
+			return;
+
+		attributes.Set( "ScreenBrightness", Brightness );
+		Graphics.GrabFrameTexture( "ColorBuffer", attributes );
+		Graphics.Blit( Material.FromShader( "shaders/postprocessing/post_example.shader" ), attributes );
+	}
+}
