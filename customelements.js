c7ae8e7a55cc76b0e68aea157abb39556ba32fff
wheatleymf <wheatleyinthespace@yandex.ru>
2025-01-19 22:23:45 +0700

    get rid of old unused mess

--- customelements.js
+++ customelements.js
@@ -15,29 +15,23 @@ class DownloadElement extends HTMLElement {
         // Container
         const container = document.createElement("div");
         container.classList.add("download");
-        container.setAttribute("part", "download");
 
         // Icon section
         const icondiv = document.createElement("div");
         icondiv.classList.add("icon");
-        icondiv.setAttribute("part", "icon");
         const iconSpan = document.createElement("span");
         iconSpan.classList.add("material-symbols-sharp");
         iconSpan.textContent = icon;
-        iconSpan.setAttribute("part", "iconstring");
         icondiv.appendChild(iconSpan);
 
         // File & title section
         const file = document.createElement("div");
         file.classList.add("file");
-        file.setAttribute("part", "file");
         const fileTitle = document.createElement("span");
         fileTitle.textContent = title;
-        fileTitle.setAttribute("part", "fileTitle");
         file.appendChild(fileTitle);
         const fileName = document.createElement("b");
         fileName.textContent = GetFilenameFromURL(link);
-        fileName.setAttribute("part", "fileName");
         file.appendChild(fileName);
 
         // Add icon/file blocks into main wrapper div
